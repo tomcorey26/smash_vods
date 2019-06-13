dd746dc794aa5b528dc25b07deb868318f2d64ba
tomcorey26 <tomcorey26@gmail.com>
2019-06-13 16:54:37 -0400

    set up shared material module

---
+++ src/app/vod-dashboard/material-ui/shared-material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatListModule } from "@angular/material/list";
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, MatListModule],
+  exports: [MatListModule],
+  providers: []
+})
+export class SharedMaterialModule {}
--- src/app/vod-dashboard/vod-dashboard.module.ts
+++ src/app/vod-dashboard/vod-dashboard.module.ts
@@ -1,7 +1,9 @@
-import { MatButtonModule, MatCheckboxModule } from "@angular/material";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { CommonModule } from "@angular/common";
+
+//material
+import { SharedMaterialModule } from "./material-ui/shared-material.module";
 //Containers
 import { VodDashboardComponent } from "./containers/vod-dashboard/vod-dashboard.component";
 
@@ -10,7 +12,7 @@ import { VodDashboardService } from "./vod-dashboard.service";
 
 @NgModule({
   declarations: [VodDashboardComponent],
-  imports: [HttpClientModule, MatButtonModule, MatCheckboxModule, CommonModule],
+  imports: [HttpClientModule, CommonModule, SharedMaterialModule],
   exports: [VodDashboardComponent],
   providers: [VodDashboardService]
 })
